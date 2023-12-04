1a56980e6acb93a1065f9b02fd43d5dace870380
Tender <tender121995@gmail.com>
2023-12-04 20:03:33 +0200

    child-component

---
+++ exercise-5/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ChildComponent from "./ChildComponent/ChildComponent";
+
+function App() {
+  return (
+    <>
+      <ChildComponent />
+    </>
+  );
+}
+
+export default App;
